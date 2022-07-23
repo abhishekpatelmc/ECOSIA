3e7db2ead91d7b9408cf75294255e5762f701fe9
Jaydutt Hiteshbhai Bhatt <jaydutt1999@gmail.com>
2022-07-23 17:01:46 -0500

    updated displayname

--- lib/screens/home/Userprofile/firestore.dart
+++ lib/screens/home/Userprofile/firestore.dart
@@ -3,15 +3,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> userSet(String name, String email, String location, String Date,
     String contact) async {
+  // var firebaseUser = await FirebaseAuth.instance.currentUser();
   CollectionReference users = FirebaseFirestore.instance.collection("users");
   FirebaseAuth auth = FirebaseAuth.instance;
   String? uid = auth.currentUser?.uid.toString();
   users.add({
+    'uid': uid,
     'name': name,
     'email': email,
     'location': location,
     'Date': Date,
-    'Conatct': contact
+    'Contact': contact
   });
   return;
 }
