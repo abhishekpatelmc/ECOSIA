26732e053e4643c85f7fca66533ed8d08efeddd6
Jaydutt Hiteshbhai Bhatt <jaydutt1999@gmail.com>
2022-07-23 15:33:04 -0500

    Display user details on firestore

--- lib/screens/home/Userprofile/firestore.dart
+++ lib/screens/home/Userprofile/firestore.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> userSet(String name, String email, String location, String Date,
     String contact) async {
-  CollectionReference users = FirebaseFirestore.instance.collection("UserData");
+  CollectionReference users = FirebaseFirestore.instance.collection("users");
   FirebaseAuth auth = FirebaseAuth.instance;
   String? uid = auth.currentUser?.uid.toString();
   users.add({
