ceab4741f39a8e686da467895e2853fa97cef516
patel4u5 <patel4u5@uwindsor.ca>
2022-07-25 16:16:35 -0400

    User Profile Page updated

--- lib/screens/home/Userprofile/UserProfile.dart
+++ lib/screens/home/Userprofile/UserProfile.dart
@@ -1,24 +1,34 @@
 // ignore_for_file: file_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecosia/screens/home/EcoCount/EcoCount.dart';
 import 'package:ecosia/screens/home/Userprofile/firestore.dart';
 import 'package:ecosia/screens/home/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ecosia/services/auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../shared/loading.dart';
 import '../../wrapper.dart';
 import '../informativepg/informativepage.dart';
+
+
 
 class UserInfo extends StatefulWidget {
   const UserInfo({Key? key}) : super(key: key);
 
+
   @override
   State<UserInfo> createState() => _UserInfoState();
+
+
 }
 
 class _UserInfoState extends State<UserInfo> {
   final AuthService _auth = AuthService();
 
+  // late Map<String, dynamic>? data;
+  String? userEmail;
   TextEditingController name = TextEditingController();
   // ignore: non_constant_identifier_names
   TextEditingController Contact = TextEditingController();
@@ -26,193 +36,238 @@ class _UserInfoState extends State<UserInfo> {
   TextEditingController enterDate = TextEditingController();
 
   @override
+  initState() {
+    userGet();
+    super.initState();
+  }
+
+
+  Future<String?> userGet() async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    if (prefs.containsKey("email")) {
+      userEmail = prefs.getString("email");
+      // print(userEmail);
+    }
+    return userEmail;
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green[300],
-        elevation: 5.0,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const UserAccountsDrawerHeader(
-              accountName: null,
-              accountEmail: null,
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/images/flag.png',
-                ),
-              ),
-            ),
-            ListTile(
-              tileColor: Colors.green[300],
-              leading: const Icon(
-                Icons.home_outlined,
-                color: Colors.white,
-              ),
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Dashboard()));
-              },
-              title: const Text(
-                "Dasboard",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              tileColor: Colors.green[300],
-              leading: const Icon(
-                Icons.info_outline_rounded,
-                color: Colors.white,
-              ),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const InformativePage()));
-              },
-              title: const Text(
-                "Informative Page",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              tileColor: Colors.green[300],
-              leading: const Icon(
-                Icons.info_outline_rounded,
-                color: Colors.white,
-              ),
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const EcoCount()));
-              },
-              title: const Text(
-                "Eco Count",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              tileColor: Colors.green[300],
-              leading: const Icon(
-                Icons.logout_outlined,
-                color: Colors.white,
-              ),
-              onTap: () async {
-                await _auth.signOut();
-                // ignore: use_build_context_synchronously
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Wrapper()),
-                  (Route<dynamic> route) => false,
-                );
-              },
-              title: const Text(
-                "Log out",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
+    // Map<String, dynamic> data = userGet() as Map<String, dynamic>;
+    // String email = userGet() as String;
+    var UserData;
+    final Stream<QuerySnapshot> _tasksStream = FirebaseFirestore.instance
+        .collection('users').where("Email", isEqualTo: userEmail)
+        .snapshots(includeMetadataChanges: true);
+
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _tasksStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+        UserData = snapshot.data!.docs[0].data()! as Map;
+        print(UserData);
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Loading();
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green[300],
+            elevation: 5.0,
+          ),
+          drawer: Drawer(
+            child: ListView(
               children: [
-                const Opacity(
-                  opacity: 0.7,
-                  child: Image(
-                    image: NetworkImage(
-                        "https://images.pexels.com/photos/2382325/pexels-photo-2382325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+
+                const UserAccountsDrawerHeader(
+                  accountName: null,
+                  accountEmail: null,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/images/flag.png',
+                    ),
                   ),
                 ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 120),
-                    child: InkWell(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(80),
-                            color: Colors.teal),
-                        height: 150,
-                        width: 150,
-                        child: const Image(
-                          image: AssetImage("assets/images/flag.png"),
-                        ),
-                      ),
-                    ),
+                ListTile(
+                  tileColor: Colors.green[300],
+                  leading: const Icon(
+                    Icons.home_outlined,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => Dashboard()));
+                  },
+                  title: const Text(
+                    "Dasboard",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ListTile(
+                  tileColor: Colors.green[300],
+                  leading: const Icon(
+                    Icons.info_outline_rounded,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const InformativePage()));
+                  },
+                  title: const Text(
+                    "Informative Page",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ListTile(
+                  tileColor: Colors.green[300],
+                  leading: const Icon(
+                    Icons.info_outline_rounded,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const EcoCount()));
+                  },
+                  title: const Text(
+                    "Eco Count",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ListTile(
+                  tileColor: Colors.green[300],
+                  leading: const Icon(
+                    Icons.logout_outlined,
+                    color: Colors.white,
+                  ),
+                  onTap: () async {
+                    await _auth.signOut();
+                    // ignore: use_build_context_synchronously
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Wrapper()),
+                          (Route<dynamic> route) => false,
+                    );
+                  },
+                  title: const Text(
+                    "Log out",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ],
             ),
-            const Center(
-                child: Text(
-              "User's name",
-              style: TextStyle(fontSize: 25, color: Colors.black),
-            )),
-            const Card(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                ),
-                title: Text("2269751234"),
-              ),
-            ),
-            const Card(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                ),
-                title: Text("user@email.com"),
-              ),
-            ),
-            const Card(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.date_range_sharp,
-                ),
-                title: Text("1999-30-06"),
-              ),
-            ),
-            const Card(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.location_city,
-                ),
-                title: Text("Windsor, Ontario"),
-              ),
-            ),
-            Padding(
-                padding: const EdgeInsets.only(top: 15, left: 20, right: 20),
-                child: InkWell(
-                  child: Container(
-                    height: 50,
-                    // width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.green[300],
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Edit User Details',
-                        style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+
+                Stack(
+                  children: [
+                    const Opacity(
+                      opacity: 0.7,
+                      child: Image(
+                        image: NetworkImage(
+                            "https://images.pexels.com/photos/2382325/pexels-photo-2382325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
                       ),
                     ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 120),
+                        child: InkWell(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(80),
+                                color: Colors.teal),
+                            height: 150,
+                            width: 150,
+                            child: const Image(
+                              image: AssetImage("assets/images/flag.png"),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                 Center(
+
+                    child: Text( UserData['name'] ?? "User Name",
+                      style: const TextStyle(fontSize: 25, color: Colors.black),
+                    )),
+                 Card(
+                  margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                    ),
+                    title: Text(UserData['Conatct'] ?? "Contact Detail"),
                   ),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const UserProfile()));
-                  },
-                )),
-          ],
-        ),
-      ),
+                ),
+                 Card(
+                  margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.email,
+                    ),
+                    title: Text(UserData['Email'] ?? "Email ID"),
+                  ),
+                ),
+                 Card(
+                  margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.date_range_sharp,
+                    ),
+                    title: Text(UserData['Date'] ?? "Date of Birth"),
+                  ),
+                ),
+                 Card(
+                  margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.location_city,
+                    ),
+                    title: Text(UserData['location'] ?? "Location Detail"),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 15, left: 20, right: 20),
+                    child: InkWell(
+                      child: Container(
+                        height: 50,
+                        // width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.green[300],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Edit User Details',
+                            style: TextStyle(fontSize: 24, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const UserProfile()));
+                      },
+                    )),
+              ],
+            ),
+          ),
+        );
+      },
     );
+
   }
 }
 
